625f537ab9df725b36b9a84da157a35600541700
Niek Versteege <niekversteege@gmail.com>
2013-01-22 15:17:56 +0100

    Prints in endpoint

--- Dare2Date_JMS/src/main/java/nl/han/dare2date/service/web/ApplyRegistrationServiceEndpoint.java
+++ Dare2Date_JMS/src/main/java/nl/han/dare2date/service/web/ApplyRegistrationServiceEndpoint.java
@@ -2,6 +2,7 @@ package nl.han.dare2date.service.web;
 
 import nl.han.dare2date.applyregistrationservice.ApplyRegistrationRequest;
 import nl.han.dare2date.applyregistrationservice.ApplyRegistrationResponse;
+import org.apache.log4j.Logger;
 
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.Unmarshaller;
@@ -10,28 +11,34 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 
 @Endpoint
 public class ApplyRegistrationServiceEndpoint {
-	private Marshaller marshaller;
-	private Unmarshaller unmarshaller;
+    
+    private Logger log = Logger.getLogger(getClass().getName());
+    private Marshaller marshaller;
+    private Unmarshaller unmarshaller;
+    
+    public ApplyRegistrationServiceEndpoint(Marshaller marshaller,
+            Unmarshaller unmarshaller) {
+        this.marshaller = marshaller;
+        this.unmarshaller = unmarshaller;
+    }
+    
+    @SuppressWarnings({"unchecked", "deprecation"})
+    @PayloadRoot(localPart = "ApplyRegistrationRequest", namespace = "http://www.han.nl/schemas/messages")
+    public ApplyRegistrationResponse applyRegistration(ApplyRegistrationRequest req) {
+        
+        System.out.println("System print test");
+        log.debug("log debug message");
+        log.info("log info message");
+        
+        ApplyRegistrationResponse ret = new ApplyRegistrationResponse();
+        boolean success = false;
+        //check if valid
 
-	public ApplyRegistrationServiceEndpoint(Marshaller marshaller,
-			Unmarshaller unmarshaller) {
-		this.marshaller = marshaller;
-		this.unmarshaller = unmarshaller;
-	}
+        //invalid -> ValidateCreditcardservice
+        //valid -> ConfirmRegistrationService
 
-	@SuppressWarnings( { "unchecked", "deprecation" })
-	@PayloadRoot(localPart = "ApplyRegistrationRequest", namespace = "http://www.han.nl/schemas/messages")
-	public ApplyRegistrationResponse applyRegistration(ApplyRegistrationRequest req) {
-            
-            ApplyRegistrationResponse ret = new ApplyRegistrationResponse();
-            boolean success = false;
-            //check if valid
-            
-            //invalid -> ValidateCreditcardservice
-            //valid -> ConfirmRegistrationService
-            
-            ret.setRegistration(req.getRegistration());
-            ret.getRegistration().setSuccesFul(success);
-            return ret;
-	}
+        ret.setRegistration(req.getRegistration());
+        ret.getRegistration().setSuccesFul(success);
+        return ret;
+    }
 }
