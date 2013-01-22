9ca410b31d2e42be97c7d9ab9585531be80ef8e2
Niek Versteege <niekversteege@gmail.com>
2013-01-22 15:29:28 +0100

    Endpoint vaagheid fix.

--- Dare2Date_JMS/src/main/java/nl/han/dare2date/service/web/ApplyRegistrationServiceEndpoint.java
+++ Dare2Date_JMS/src/main/java/nl/han/dare2date/service/web/ApplyRegistrationServiceEndpoint.java
@@ -11,30 +11,15 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 @Endpoint
 public class ApplyRegistrationServiceEndpoint {
     
-    private Logger log = Logger.getLogger(getClass().getName());
-    private Marshaller marshaller;
-    private Unmarshaller unmarshaller;
-    
-    public ApplyRegistrationServiceEndpoint(Marshaller marshaller,
-            Unmarshaller unmarshaller) {
-        this.marshaller = marshaller;
-        this.unmarshaller = unmarshaller;
-    }
-    
-    @SuppressWarnings({"unchecked", "deprecation"})
-    @PayloadRoot(localPart = "ApplyRegistrationRequest", namespace = "http://www.han.nl/schemas/messages")
-    public ApplyRegistrationResponse applyRegistration(ApplyRegistrationRequest req) {
-        
-        System.out.println("System print test");
-        log.debug("log debug message");
-        log.info("log info message");
-        
-        ApplyRegistrationResponse ret = new ApplyRegistrationResponse();
-        boolean success = false;
-        //check if valid
+        private Logger log = Logger.getLogger(getClass().getName());
+	private Marshaller marshaller;
+	private Unmarshaller unmarshaller;
 
-        //invalid -> ValidateCreditcardservice
-        //valid -> ConfirmRegistrationService
+	public ApplyRegistrationServiceEndpoint(Marshaller marshaller,
+			Unmarshaller unmarshaller) {
+		this.marshaller = marshaller;
+		this.unmarshaller = unmarshaller;
+	}
 
 	@PayloadRoot(localPart = "ApplyRegistrationRequest", namespace = "http://www.han.nl/schemas/messages")
 	public ApplyRegistrationResponse applyRegistration(ApplyRegistrationRequest req) {
