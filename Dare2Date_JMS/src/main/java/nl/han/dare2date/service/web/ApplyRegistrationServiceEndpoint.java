3696995f15024a2827b1083b271ea8f6c7fb53f7
kaaz <kaaz@145.74.158.229>
2013-01-22 15:15:53 +0100

    serialize

--- Dare2Date_JMS/src/main/java/nl/han/dare2date/service/web/ApplyRegistrationServiceEndpoint.java
+++ Dare2Date_JMS/src/main/java/nl/han/dare2date/service/web/ApplyRegistrationServiceEndpoint.java
@@ -2,7 +2,7 @@ package nl.han.dare2date.service.web;
 
 import nl.han.dare2date.applyregistrationservice.ApplyRegistrationRequest;
 import nl.han.dare2date.applyregistrationservice.ApplyRegistrationResponse;
-
+import org.apache.log4j.Logger;
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -10,6 +10,8 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 
 @Endpoint
 public class ApplyRegistrationServiceEndpoint {
+    
+        private Logger log = Logger.getLogger(getClass().getName());
 	private Marshaller marshaller;
 	private Unmarshaller unmarshaller;
 
@@ -19,11 +21,13 @@ public class ApplyRegistrationServiceEndpoint {
 		this.unmarshaller = unmarshaller;
 	}
 
-	@SuppressWarnings( { "unchecked", "deprecation" })
 	@PayloadRoot(localPart = "ApplyRegistrationRequest", namespace = "http://www.han.nl/schemas/messages")
 	public ApplyRegistrationResponse applyRegistration(ApplyRegistrationRequest req) {
             
+            log.debug("HALLO????");
+            System.out.println("DE PRINT FUNCTIE WOEI");
             ApplyRegistrationResponse ret = new ApplyRegistrationResponse();
+            
             boolean success = false;
             //check if valid
             
